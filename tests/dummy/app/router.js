31df201a5eacd0a2fc96c03fda004152599a4e8a
achraf <achrafatauil@hotmail.com>
2020-08-24 16:27:33 +0200

    change path gen-class-route from app to addon

--- tests/dummy/app/router.js
+++ tests/dummy/app/router.js
@@ -1,7 +1,7 @@
 import EmberRouter from '@ember/routing/router';
 import config from 'dummy/config/environment';
 import metisFallbackRoute from 'metis/utils/fallback-route';
-import GCR from './utils/gen-class-route';
+import GCR from 'metis/utils/gen-class-route';
 
 export default class Router extends EmberRouter {
   location = config.locationType;

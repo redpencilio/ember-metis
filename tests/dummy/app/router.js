22d0e4dc1e16f7b09016e2477a2c972808f44203
achraf <achrafatauil@hotmail.com>
2020-08-24 13:58:05 +0200

    add class route generator code to dummy

--- tests/dummy/app/router.js
+++ tests/dummy/app/router.js
@@ -1,6 +1,7 @@
 import EmberRouter from '@ember/routing/router';
 import config from 'dummy/config/environment';
 import metisFallbackRoute from 'metis/utils/fallback-route';
+import GCR from './utils/gen-class-route';
 
 export default class Router extends EmberRouter {
   location = config.locationType;
@@ -8,5 +9,11 @@ export default class Router extends EmberRouter {
 }
 
 Router.map(function() {
-  metisFallbackRoute(this);  
+  this.route('view', function() {
+    const classRoute = GCR("view", this);
+
+    classRoute('person', { class: "http://www.w3.org/ns/person#Person" } );
+  });
+  
+  metisFallbackRoute(this);
 });

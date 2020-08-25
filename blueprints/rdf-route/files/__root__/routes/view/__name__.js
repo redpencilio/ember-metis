b32b1d23b1afae5a5d6b1d56942221dadf630781
achraf <achrafatauil@hotmail.com>
2020-08-25 12:03:55 +0200

    Dynamically add controller class name

--- blueprints/rdf-route/files/__root__/routes/view/__name__.js
+++ blueprints/rdf-route/files/__root__/routes/view/__name__.js
@@ -1,7 +1,7 @@
 import Route from '@ember/routing/route';
 
 // Need to dynamically change class name to actual generator argument
-export default class ViewPersonRoute extends Route {
+export default class View<%= classifiedModuleName %>Route extends Route {
   queryParams = {
     resource: { refreshModel: true }
   }

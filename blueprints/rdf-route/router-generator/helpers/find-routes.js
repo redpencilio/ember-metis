8e488b4530d210404091c29bcdba9a900888096d
achraf <achrafatauil@hotmail.com>
2020-08-28 13:14:46 +0200

    add comments + addAction parameter
    the add & remove functions share same find-routes function so we need addAction to do different actions for each action

--- blueprints/rdf-route/router-generator/helpers/find-routes.js
+++ blueprints/rdf-route/router-generator/helpers/find-routes.js
@@ -2,29 +2,33 @@
 var recast = require('recast');
 
 // Checks if route already exists
-module.exports = function findRoutes(name, routes, identifier) {
+module.exports = function findRoutes(name, routes, identifier, addAction) {
   var routeFound = false // default
 
-  console.log(identifier)
   recast.visit(routes, {
     visitExpressionStatement: function(path) {
       var node = path.node;
 
       // Checks if route exists 
       if (node.expression.callee.name == "classRoute" && node.expression.arguments[0].value == name) {
-
+        // Route is found so set to true
         routeFound = true
 
-        // If route exists then update the property value to the vocabulary given by the user
-        node.expression.arguments[1].properties[0].value = `'${identifier}'`
+        // Check if findRoutes function is called by ADD & not REMOVE
+        if(addAction){
+          // If route exists then update the property value to the vocabulary given by the user
+          node.expression.arguments[1].properties[0].value = `'${identifier}'`
+        }
 
+        // Exit loop
         return false;
         
       } else {
+        // Loop other nodes
         this.traverse(path);
       }
     }
   });
 
-  return  routeFound ;
+  return routeFound ;
 };

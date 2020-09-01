166e40a2630ce8a0c4814821ddc08c7c1ff0ab6b
achraf <achrafatauil@hotmail.com>
2020-09-01 15:33:20 +0200

    Fix export issue + forgotten '$'
    Removed default exports (there where 2 of them in one file) and replaced them by named exports

--- addon/utils/class-route.js
+++ addon/utils/class-route.js
@@ -1,8 +1,10 @@
+
+
 import config from 'ember-get-config';
 const { metis } = config;
 
 
-export default function genClassRoute(basePath, self) {
+function genClassRoute(basePath, self) {
 
   return function(name, options){
     let route;
@@ -18,11 +20,12 @@ export default function genClassRoute(basePath, self) {
     return this.route(name, options);
   }.bind(self);
 }
-export default function genClassRoute(route, name, options) {
+
+function classRoute(route, name, options) {
   // Calculate target route
   let routeString;
   if( route.parent !== "application" ) {
-    routeString = `${route.parent}.{name}`;
+    routeString = `${route.parent}.${name}`;
   } else {
     routeString = name;
   }
@@ -34,3 +37,6 @@ export default function genClassRoute(route, name, options) {
   // Create new route
   return route.route(name, options);
 }
+
+export { genClassRoute, classRoute }
+

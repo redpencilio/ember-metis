51cbef2110bb606c00a2f16498cd6b5a6fd56029
achraf <achrafatauil@hotmail.com>
2020-09-01 15:35:09 +0200

    remove unnecessary comment

--- blueprints/rdf-route/files/__root__/routes/view/__name__.js
+++ blueprints/rdf-route/files/__root__/routes/view/__name__.js
@@ -1,6 +1,5 @@
 import Route from '@ember/routing/route';
 
-// Need to dynamically change class name to actual generator argument
 export default class View<%= classifiedModuleName %>Route extends Route {
   queryParams = {
     resource: { refreshModel: true }

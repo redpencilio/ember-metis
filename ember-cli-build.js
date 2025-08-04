8b7ff1943be0f13432a767d8c0f9790320f8fc53
Jakub Szymsza <jakub@szymsza.cz>
2025-08-04 15:48:13 +0200

    style: lint

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -5,9 +5,7 @@ const EmberAddon = require('ember-cli/lib/broccoli/ember-addon');
 module.exports = function (defaults) {
   const app = new EmberAddon(defaults, {
     sassOptions: {
-      includePaths: [
-        'node_modules/@appuniversum/ember-appuniversum/styles',
-      ],
+      includePaths: ['node_modules/@appuniversum/ember-appuniversum/styles'],
     },
     '@appuniversum/ember-appuniversum': {
       disableWormholeElement: true,

e5857876b93f337c6641cc3986d7085a4e51edb4
Jakub Szymsza <jakub@szymsza.cz>
2025-08-25 15:42:01 +0200

    chore: purge appuniversum from the project

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -7,9 +7,6 @@ module.exports = function (defaults) {
     sassOptions: {
       includePaths: ['./styles'],
     },
-    '@appuniversum/ember-appuniversum': {
-      disableWormholeElement: true,
-    },
   });
 
   app.import('node_modules/svgxuse/svgxuse.js');
